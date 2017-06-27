9d26bb937c0baba373da296d00e9fc9730ad3edb
Alan White <alanwhitey@gmail.com>
2017-06-27 16:24:57 +0100

    initial commit

---
+++ Photographer.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Photographer{
+
+  private String name;
+
+  private ArrayList cameras;
+
+  public Photographer(String name){
+    this.name = name;
+    this.cameras = new ArrayList
+
+  }
+
+
+}
